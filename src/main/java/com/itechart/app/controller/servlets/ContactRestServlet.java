52ec946f6872862be981d3890420415c8839d619
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-09-11 00:00:04 +0300

    add class ContactRestServlet

---
+++ src/main/java/com/itechart/app/controller/servlets/ContactRestServlet.java
@@ -0,0 +1,69 @@
+package com.itechart.app.controller.servlets;
+
+import com.itechart.app.controller.utils.RequestContent;
+import com.itechart.app.controller.utils.RestRequest;
+import com.itechart.app.model.actions.ContactAction;
+import com.itechart.app.model.factories.ContactActionFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ContactRestServlet extends HttpServlet{
+
+    private static final String RESOURCE_ID_ATTRIBUTE = "resourceId";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Create RestRequest object to handle REST request
+        RestRequest restRequest = new RestRequest(request.getPathInfo());
+
+        // Create wrapper for HttpServletRequest for getting params from JSP
+        // and inserting attributes back to JSP
+        RequestContent requestContent = new RequestContent(request);
+
+        // Check if the rest request contains resource id and if so
+        // add this attribute to RequestContent
+        if(restRequest.hasResourceId()){
+            requestContent.insertAttribute(RESOURCE_ID_ATTRIBUTE, restRequest.getResourceId());
+        }
+
+        // identify command coming from JSP
+        ContactActionFactory factory = new ContactActionFactory();
+        ContactAction action = factory.defineContactAction(restRequest.getActionFromRestUrl());
+
+        // handle request according type of action
+        String pagePath = action.execute(requestContent);
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagePath);
+        dispatcher.forward(request, response);
+    }
+}
