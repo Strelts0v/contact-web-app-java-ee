9ec7311350a65112ed013c2caab760917d976c34
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-09-11 00:14:51 +0300

    add class GmailEmailService

---
+++ src/main/java/com/itechart/app/model/email/GmailEmailService.java
@@ -0,0 +1,66 @@
+package com.itechart.app.model.email;
+
+import com.itechart.app.model.cryptography.Cryptographer;
+import com.itechart.app.model.cryptography.CryptographerXor;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * implementation of EmailService interface using gmail via TLS
+ */
+public class GmailEmailService implements EmailService {
+
+    /**
+     * java mail session object
+     */
+    private static Session SESSION;
+
+    /**
+     * object for retrieving mail settings
+     */
+    private static Properties PROPERTIES;
+
+    /**
+     * initializes session and properties objects for getting ready sending emails
+     */
+    public GmailEmailService() {
+        InputStream input = getClass().getClassLoader().getResourceAsStream("mail.properties");
+        try {
+            PROPERTIES = new Properties();
+            PROPERTIES.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Cryptographer cryptographer = new CryptographerXor();
+
+        SESSION = Session.getInstance(PROPERTIES,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(
+                                PROPERTIES.getProperty("mail.username"),
+                                cryptographer.decrypt(PROPERTIES.getProperty("mail.password"))
+                        );
+                    }
+                });
+    }
+
+    @Override
+    public void sendMessage(String fromEmail, String toEmail, String subject, String messageText) {
+        try {
+            Message message = new MimeMessage(SESSION);
+            message.setFrom(new InternetAddress(fromEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject(subject);
+            message.setText(messageText);
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
