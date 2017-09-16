62bc7cd6d92cd4012708d8d97c93bfbe6e2feb16
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-09-16 20:58:18 +0300

    add class AddAttachmentToContactAction

---
+++ src/main/java/com/itechart/app/model/actions/AddAttachmentToContactAction.java
@@ -0,0 +1,10 @@
+package com.itechart.app.model.actions;
+
+import com.itechart.app.controller.utils.RequestContent;
+
+public class AddAttachmentToContactAction implements ContactAction{
+
+    public String execute(RequestContent requestContent) {
+        return null;
+    }
+}
