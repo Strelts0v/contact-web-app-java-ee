7c6307214a789732d0f7489c92c82e9d6a38db16
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-09-17 20:05:49 +0300

    update class GetContactsAction

--- src/main/java/com/itechart/app/model/actions/GetContactsAction.java
+++ src/main/java/com/itechart/app/model/actions/GetContactsAction.java
@@ -9,42 +9,42 @@ import java.util.List;
 
 public class GetContactsAction implements ContactAction{
 
-    private final static String OFFSET_PARAM_NAME = "offset";
-
-    private final static String COUNT_PARAM_NAME = "count";
+    private final static String CONTACT_PAGE_PARAM_NAME = "page";
 
     private final static String CONTACTS_ATTRIBUTE_NAME = "contactList";
+
+    private final static String CONTACT_COUNT_ATTRIBUTE_NAME = "pageCount";
 
     private final static String ERROR_PAGE_NAME = "path.page.jsp.error";
 
     private final static String CONTACT_LIST_PAGE_NAME = "path.page.jsp.contact-list";
 
-    private final static int DEFAULT_OFFSET = 0;
-
-    private final static int DEFAULT_COUNT = 20;
+    private final static int DEFAULT_CONTACT_COUNT = 20;
 
     public String execute(RequestContent requestContent) {
         String page;
 
-        int offset = Integer.parseInt(requestContent.getParameter(OFFSET_PARAM_NAME));
-        int count = Integer.parseInt(requestContent.getParameter(COUNT_PARAM_NAME));
-
-        if(count == 0){
-            offset = DEFAULT_OFFSET;
-            count = DEFAULT_COUNT;
-        }
+        final String pageIndexStr = requestContent.getParameter(CONTACT_PAGE_PARAM_NAME);
+        final int pageIndex = Integer.parseInt(pageIndexStr);
+        final int offset = (pageIndex - 1) * DEFAULT_CONTACT_COUNT;
 
         JdbcContactDao dao = JdbcContactDao.newInstance();
         if(dao == null){
             page = PageConfigurationManager.getPageName(ERROR_PAGE_NAME);
         } else {
-            List<Contact> contactList = dao.getContacts(offset, count);
+            int contactCount = dao.getContactCount();
+            List<Contact> contactList = dao.getContacts(offset, DEFAULT_CONTACT_COUNT);
             requestContent.insertAttribute(CONTACTS_ATTRIBUTE_NAME, contactList);
+
+            int pageCount = contactCount % DEFAULT_CONTACT_COUNT == 0
+                    ? contactCount / DEFAULT_CONTACT_COUNT
+                    : contactCount / DEFAULT_CONTACT_COUNT + 1;
+
+            requestContent.insertAttribute(CONTACT_COUNT_ATTRIBUTE_NAME, pageCount);
 
             page = PageConfigurationManager.getPageName(CONTACT_LIST_PAGE_NAME);
             dao.closeConnection();
         }
-
         return page;
     }
 }
