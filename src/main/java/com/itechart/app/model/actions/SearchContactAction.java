89f797e0837862e89221d6d294af05ab8ba6b6a6
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-09-24 12:03:32 +0300

    update class SearchContactAction

--- src/main/java/com/itechart/app/model/actions/SearchContactAction.java
+++ src/main/java/com/itechart/app/model/actions/SearchContactAction.java
@@ -1,10 +1,25 @@
 package com.itechart.app.model.actions;
 
 import com.itechart.app.controller.utils.RequestContent;
+import com.itechart.app.model.utils.PageConfigurationManager;
 
 public class SearchContactAction implements ContactAction{
 
+    // param to identify client want only get page with form
+    // or proceed searching
+    private static final String CONTACT_SUBMIT_PARAM = "submit";
+
+    private static final String SEARCH_CONTACT_PAGE_NAME = "path.page.jsp.search";
+
     public String execute(RequestContent requestContent) {
-        return null;
+        String page;
+
+        Boolean isSubmit = Boolean.valueOf(requestContent.getParameter(CONTACT_SUBMIT_PARAM));
+        if(!isSubmit){
+            page = PageConfigurationManager.getPageName(SEARCH_CONTACT_PAGE_NAME);
+        } else {
+            page = null;
+        }
+        return page;
     }
 }
