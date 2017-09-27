dfe3b6d9b51208fcde90ab93618eed9b7d43c9c2
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-09-27 09:34:46 +0300

    update class ErrorContactAction

--- src/main/java/com/itechart/app/model/actions/ErrorContactAction.java
+++ src/main/java/com/itechart/app/model/actions/ErrorContactAction.java
@@ -1,10 +1,13 @@
 package com.itechart.app.model.actions;
 
 import com.itechart.app.controller.utils.RequestContent;
+import com.itechart.app.model.utils.PageConfigurationManager;
 
 public class ErrorContactAction implements ContactAction{
 
+    private final static String ERROR_PAGE_NAME = "path.page.jsp.error";
+
     public String execute(RequestContent requestContent) {
-        return null;
+        return PageConfigurationManager.getPageName(ERROR_PAGE_NAME);
     }
 }
