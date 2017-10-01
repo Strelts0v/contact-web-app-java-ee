a1c6d3eeda164edc4d6e1ea8a1808e4c971cb1f7
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-10-01 12:17:05 +0300

    refactor class SendEmailToContactsAction

--- src/main/java/com/itechart/app/model/actions/SendEmailToContactsAction.java
+++ src/main/java/com/itechart/app/model/actions/SendEmailToContactsAction.java
@@ -1,22 +1,17 @@
 package com.itechart.app.model.actions;
 
 import com.itechart.app.controller.utils.RequestContent;
+import com.itechart.app.model.actions.utils.ContactActionProperties;
 import com.itechart.app.model.utils.PageConfigurationManager;
 
 public class SendEmailToContactsAction implements ContactAction{
 
-    // param to identify client want only get page with form
-    // or proceed searching
-    private static final String EMAIL_SUBMIT_PARAM = "submit";
-
-    private static final String EMAIL_PAGE_NAME = "path.page.jsp.email";
-
     public String execute(RequestContent requestContent) {
         String page;
 
-        Boolean isSubmit = Boolean.valueOf(requestContent.getParameter(EMAIL_SUBMIT_PARAM));
+        Boolean isSubmit = Boolean.valueOf(requestContent.getParameter(ContactActionProperties.EMAIL_SUBMIT_PARAM));
         if(!isSubmit){
-            page = PageConfigurationManager.getPageName(EMAIL_PAGE_NAME);
+            page = PageConfigurationManager.getPageName(ContactActionProperties.EMAIL_PAGE_NAME);
         } else {
             page = null;
         }
