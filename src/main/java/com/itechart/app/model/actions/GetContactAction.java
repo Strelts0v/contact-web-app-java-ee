fb3459a1ad9c364caac1429a5a0fb0f68424eb96
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-10-01 12:16:19 +0300

    refactor class GetContactAction

--- src/main/java/com/itechart/app/model/actions/GetContactAction.java
+++ src/main/java/com/itechart/app/model/actions/GetContactAction.java
@@ -2,6 +2,7 @@ package com.itechart.app.model.actions;
 
 import com.itechart.app.controller.utils.RequestContent;
 import com.itechart.app.logging.AppLogger;
+import com.itechart.app.model.actions.utils.ContactActionProperties;
 import com.itechart.app.model.dao.ContactDao;
 import com.itechart.app.model.dao.JdbcContactDao;
 import com.itechart.app.model.entities.Contact;
@@ -10,33 +11,25 @@ import com.itechart.app.model.utils.PageConfigurationManager;
 
 public class GetContactAction implements ContactAction{
 
-    private static final String CONTACT_ATTRIBUTE_ID = "contactId";
-
-    private static final String CONTACT_ATTRIBUTE_NAME = "contact";
-
-    private final static String ERROR_PAGE_NAME = "path.page.jsp.error";
-
-    private final static String CONTACT_DETAIL_PAGE_NAME = "path.page.jsp.contact-detail";
-
     public String execute(RequestContent requestContent) {
         String page;
 
-        final int contactId = (int) requestContent.getAttribute(CONTACT_ATTRIBUTE_ID);
+        final int contactId = (int) requestContent.getAttribute(ContactActionProperties.CONTACT_ATTRIBUTE_ID);
 
         try {
             ContactDao dao = JdbcContactDao.newInstance();
             if (dao == null) {
-                page = PageConfigurationManager.getPageName(ERROR_PAGE_NAME);
+                page = PageConfigurationManager.getPageName(ContactActionProperties.ERROR_PAGE_NAME);
             } else {
                 Contact contact = dao.getContact(contactId);
-                requestContent.insertAttribute(CONTACT_ATTRIBUTE_NAME, contact);
+                requestContent.insertAttribute(ContactActionProperties.CONTACT_ATTRIBUTE_NAME, contact);
 
-                page = PageConfigurationManager.getPageName(CONTACT_DETAIL_PAGE_NAME);
+                page = PageConfigurationManager.getPageName(ContactActionProperties.CONTACT_DETAIL_PAGE_NAME);
                 dao.closeDao();
             }
         } catch (ContactDaoException cde){
             AppLogger.error(cde.getMessage());
-            page = PageConfigurationManager.getPageName(ERROR_PAGE_NAME);
+            page = PageConfigurationManager.getPageName(ContactActionProperties.ERROR_PAGE_NAME);
         }
         return page;
     }
