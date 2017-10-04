fa4ed8a5b8ef1ae4c2ee2ffc14258995d08a7d4a
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-10-04 16:01:15 +0300

    update class ErrorContactAction

--- src/main/java/com/itechart/app/model/actions/ErrorContactAction.java
+++ src/main/java/com/itechart/app/model/actions/ErrorContactAction.java
@@ -1,15 +1,18 @@
 package com.itechart.app.model.actions;
 
 import com.itechart.app.controller.utils.RequestContent;
-import com.itechart.app.logging.AppLogger;
 import com.itechart.app.model.actions.utils.ContactActionProperties;
 import com.itechart.app.model.utils.PageConfigurationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ErrorContactAction implements ContactAction{
 
+    private final Logger logger = LoggerFactory.getLogger(ErrorContactAction.class);
+
     public String execute(RequestContent requestContent) {
         String page = PageConfigurationManager.getPageName(ContactActionProperties.ERROR_PAGE_NAME);
-        AppLogger.info("Return " + page + " to client");
+        logger.info("Return " + page + " to client");
         return page;
     }
 }
