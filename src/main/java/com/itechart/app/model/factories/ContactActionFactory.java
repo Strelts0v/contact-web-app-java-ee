6c79fb4be93356bb6fb69aa3bca3da2b3a33f5b2
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-10-04 16:05:40 +0300

    update class ContactActionFactory

--- src/main/java/com/itechart/app/model/factories/ContactActionFactory.java
+++ src/main/java/com/itechart/app/model/factories/ContactActionFactory.java
@@ -1,16 +1,19 @@
 package com.itechart.app.model.factories;
 
-import com.itechart.app.logging.AppLogger;
 import com.itechart.app.model.actions.ContactAction;
 import com.itechart.app.model.enums.ContactActionEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ContactActionFactory {
+
+    private final Logger logger = LoggerFactory.getLogger(ContactActionFactory.class);
 
     public ContactAction defineContactAction(final String actionFromRestUrl){
         ContactActionEnum contactActionEnum;
         try{
             contactActionEnum = ContactActionEnum.valueOf(actionFromRestUrl.toUpperCase());
-            AppLogger.info(contactActionEnum.getCurrentContactAction().getClass() +
+            logger.info(contactActionEnum.getCurrentContactAction().getClass() +
                     " instance is successfully returned from ContactActionFactory");
         } catch (IllegalArgumentException e) {
             contactActionEnum = ContactActionEnum.ERROR;
