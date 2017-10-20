ade7bb0d8e4023dbc7428051f254088696b0120f
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-10-20 12:42:42 +0300

    fix issue with uploading contact avatar

--- src/main/java/com/itechart/app/model/actions/GetContactAction.java
+++ src/main/java/com/itechart/app/model/actions/GetContactAction.java
@@ -7,8 +7,10 @@ import com.itechart.app.model.dao.JdbcContactDao;
 import com.itechart.app.model.entities.Contact;
 import com.itechart.app.model.exceptions.ContactDaoException;
 import com.itechart.app.model.utils.PageConfigurationManager;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.Base64;
 
 public class GetContactAction implements ContactAction{
 
@@ -27,19 +29,25 @@ public class GetContactAction implements ContactAction{
             } else {
                 Contact contact = dao.getContact(contactId);
                 requestContent.insertAttribute(ContactActionProperties.CONTACT_ATTRIBUTE_NAME, contact);
+                if(contact.getPhoto().getPhotoStream() != null) {
+                    byte[] photoData = IOUtils.toByteArray(contact.getPhoto().getPhotoStream());
+                    String encodedPhoto = Base64.getEncoder().encodeToString(photoData);
+                    requestContent.insertAttribute(ContactActionProperties.CONTACT_ATTRIBUTE_IMAGE,
+                            encodedPhoto);
+                }
 
                 page = PageConfigurationManager.getPageName(ContactActionProperties.CONTACT_DETAIL_PAGE_NAME);
                 dao.closeDao();
                 logger.info("Getting of contact with id=" + contact.getContactId() + " was successful");
             }
-        } catch (ContactDaoException cde){
-            logger.error(cde.getMessage());
+        } catch (Exception e){
+            logger.error(e.getMessage());
             try {
                 if(dao != null) {
                     dao.closeDao();
                 }
-            } catch (ContactDaoException cdex){
-                logger.error(cde.getMessage());
+            } catch (ContactDaoException ex){
+                logger.error(e.getMessage());
             }
             page = PageConfigurationManager.getPageName(ContactActionProperties.ERROR_PAGE_NAME);
         }
